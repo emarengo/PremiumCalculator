bc2c0c0d83d8a28c1e01a26b67dfd6aa297baad7
emarengo <64386038+emarengo@users.noreply.github.com>
2022-11-23 16:40:40 -0500

    feat: adds information and descriptions

--- app/src/controllers/404.controller.js
+++ app/src/controllers/404.controller.js
@@ -1,6 +1,11 @@
 import notFound from "../views/404.html"
+import "../views/404.scss"
+
+const loader = document.getElementById("spinner");
 
 export default () => {
+
+    loader.style.display = "none";
     const divElement = document.createElement("div")
     divElement.innerHTML = notFound
     return divElement
--- app/src/controllers/index.controller.js
+++ app/src/controllers/index.controller.js
@@ -1,5 +1,11 @@
 import home from "../views/home.html"
+import "../views/home.scss"
+
+const loader = document.getElementById("spinner");
+
 export default () => {
+    loader.style.display = "none";
+
     const divElement = document.createElement("div")
     divElement.innerHTML = home
     return divElement
